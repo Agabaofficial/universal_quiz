b1df5182ad09a53fdb74e6847b0513de082eb595
Agaba Olivier <125471253+Agabaofficial@users.noreply.github.com>
2024-03-28 15:38:44 +0300

    updated logging in

--- endpoint/login.php
+++ endpoint/login.php
@@ -9,29 +9,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->bindParam(':username', $username);
     $stmt->execute();
 
-    if ($password === $stored_password) {
-        echo "
-        <script>
-            alert('Login Successfully!');
-            window.location.href = 'http://localhost/codetester/universal.html';
-        </script>
-        "; 
+    if ($stmt->rowCount() > 0) {
+        $row = $stmt->fetch();
+        $stored_password = $row['password'];
+
+        if ($password === $stored_password) {
+            echo "
+            <script>
+                alert('Login Successfully!');
+                window.location.href = 'http://localhost/codetester/universal.html';
+            </script>
+            "; 
+        } else {
+            echo "
+            <script>
+                alert('Login Failed, Incorrect Password!');
+                window.location.href = 'http://localhost/index.php';
+            </script>
+            ";
+        }
     } else {
         echo "
-        <script>
-            alert('Login Failed, Incorrect Password!');
-            window.location.href = 'http://localhost/home.php';
-        </script>
-        ";
+            <script>
+                alert('Login Failed, User Not Found!');
+                window.location.href = 'http://localhost/codetester/endpoint/login.php';
+            </script>
+            ";
     }
-} else {
-    echo "
-        <script>
-            alert('Login Failed, User Not Found!');
-            window.location.href = 'http://localhost/home.php';
-        </script>
-        ";
-}
 }
 
 ?>
